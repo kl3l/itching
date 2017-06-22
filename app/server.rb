c5514fc06ef0a2bff12684e93115592e974fb3e6
ctrembath <claretrembath@ymail.com>
2017-06-22 17:10:57 +0100

    email

--- app/server.rb
+++ app/server.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
+require 'dotenv/load'
 require 'pony'
 
 set :public_dir, Proc.new { File.join(root, "..", "public") }
+
+class Application < Sinatra::Base
+  configure do
+    Pony.options = {
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :domain => 'myapp.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
+    }
+  end
+end
 
 get '/' do
   erb :index
@@ -20,9 +38,15 @@ get '/qz' do
 end
 
 post '/qz' do
-  params[:name]
+  Pony.mail(
+    to: 'clare@no-mayo.co.uk',
+    from: "Itching",
+    subject: "itching email",
+    :headers => { 'Content-Type' => 'text/html' },
+    body: "<h2>from : #{params[:name]}</h2>" + "<br>" + "What you know bout me? #{params[:knowboutme]}" + "<br>" + "did you drive past clare on the m4? : " + params[:zone]
+  )
+  redirect '/'
 end
-
 
 get '/gif' do
   erb :gif
